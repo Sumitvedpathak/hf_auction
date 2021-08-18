2557c92e09ceb6a963115bf79ccb3cc16dfb9b42
Sumit Vedpathak <sumitvedpathak@gmail.com>
2021-08-18 07:58:05 -0400

    updates

--- chaincode/lib/auction.js
+++ chaincode/lib/auction.js
@@ -9,7 +9,7 @@ class AuctionContract extends Contract {
     async _putState(ctx, bid) {
         const bidCompKey = ctx.stub.createCompositeKey(bidObjectType,[bid.id]);
         const bidValue = Buffer.from(JSON.stringify(bid));
-        console.log("Bid Composite Key - " + bidCompKey + "Bid Value - " + bidValue);
+        console.log("Bid Composite Key - " + bidCompKey + ", Bid Value - " + bidValue);
         await ctx.stub.putState(bidCompKey,bidValue);
     }
 
@@ -17,9 +17,21 @@ class AuctionContract extends Contract {
         const bid = {
             id : id,
             value : value,
-            bidder : ctx.clientIdentity.getId()
+            bidder : ctx.clientIdentity.getID()
         };
         await this._putState(ctx,bid);
+        console.log("Bid Created Successfully!");
+    }
+
+    async  listBids(ctx){
+        const iterator = ctx.stub.getStateByPartialCompositeKey(bidObjectType,[]);
+        let result = [];
+        for await(const itr of iterator) {
+            const bidVal = JSON.parse(itr.value.toString('utf8'));
+            result.push(bidVal);
+        }
+        console.log(result);
+        return JSON.stringify(result);
     }
 
 }
